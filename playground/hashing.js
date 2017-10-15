f437313bf511c6485477dde23d57c4a8de887a45
Goose <josephtrop@gmail.com>
2017-10-15 14:16:57 -0700

    storing hashed passwords

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,30 @@
 const {SHA256} =  require('crypto-js')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 
-var data = {
-    id:10
-}
-//123abc some secret stuff
-var token = jwt.sign(data,'123abc') //verify jwt.io
-console.log( token )
+var password = '123abc'
+               //rounds
+bcrypt.genSalt(10,(err,salt)=>{
+    bcrypt.hash(password,salt,(err,hash)=>{
+        console.log(hash)
+    })
+})
 
-var decoded =  jwt.verify(token,'123abc')
-console.log( 'decoded', decoded)
+var hashPassword = '$2a$10$4VO05wxERNlimPUiCXJTOe4mCPsd1I0oFMZmIQI1z2Xm6NS1zhupK'
+
+bcrypt.compare(password,hashPassword,(err,result)=>{
+    console.log(result)
+})
+
+// var data = {
+//     id:10
+// }
+// //123abc some secret stuff
+// var token = jwt.sign(data,'123abc') //verify jwt.io
+// console.log( token )
+
+// var decoded =  jwt.verify(token,'123abc')
+// console.log( 'decoded', decoded)
 
 
 // var message =  'I am number 3'
